af03ab433409f902b54b3cd3ae8e3b9e4041416f
Bas Kaal <3askaal@gmail.com>
2022-07-01 00:12:30 +0200

    fix styling for smallest screens

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,7 +2,7 @@ import { colors } from './colors'
 import { fonts } from './fonts'
 
 export const localTheme = {
-  rootFontSizes: ['10px', '14px', '16px', '18px'],
+  rootFontSizes: ['8px', '14px', '16px', '18px'],
   colors,
   fonts
 }
