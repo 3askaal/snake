587e931a1904c0567198c663a28780e0e4478da8
Bas Kaal <3askaal@gmail.com>
2022-06-30 23:14:12 +0200

    remove unused code

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -4,38 +4,5 @@ import { fonts } from './fonts'
 export const localTheme = {
   rootFontSizes: ['10px', '14px', '16px', '18px'],
   colors,
-  fonts,
-  components: {
-    Input: {
-      default: {
-        padding: 'xs',
-        borderWidth: '1px'
-      },
-      variants: {
-        isBlock: {
-          width: '100% !important'
-        }
-      }
-    },
-    Button: {
-      default: {
-        paddingX: 's',
-        paddingY: 'xs',
-        borderWidth: '2px',
-      },
-      variants: {
-        isReady: {
-          backgroundColor: 'transparent',
-          color: 'positive',
-          borderColor: 'positive',
-
-          ':hover': {
-            backgroundColor: 'positive',
-            color: 'background',
-            borderColor: 'positive',
-          }
-        }
-      }
-    },
-  },
+  fonts
 }
